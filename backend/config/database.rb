d0c83be82bb909a756fd749af36340568b9978ec
aidewoode <aidewoode@gmail.com>
2015-10-04 23:04:43 +0800

    add some basic migrations

--- backend/config/database.rb
+++ backend/config/database.rb
@@ -1,7 +1,8 @@
 Sequel::Model.plugin(:schema)
+Sequel::Model.plugin(:timestamps, update_on_create: true)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("postgres:///backend_development", :loggers => [logger])
+  when :development then Sequel.connect("postgres:///simple_forum_backend_development", :loggers => [logger])
   when :production  then Sequel.connect(ENV['DATABASE_URL'], :loggers => [logger])
-  when :test        then Sequel.connect("postgres:///backend_test",  :loggers => [logger])
+  when :test        then Sequel.connect("postgres:///simple_forum_backend_test",  :loggers => [logger])
 end
